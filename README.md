8dfadb5032bed24895ff88f64ca2153b193ea2fa
veritascoding <jarrettcoding@gmail.com>
2021-11-28 14:47:43 -0600

    finalize changes and create README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# password-generator
+# Password Generator
+
+## Purpose
+Generate randomized password based on user option choices
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://jarrettcoding.github.io/password-generator/
+
+## Contribution
+Made with ❤️ by Jarrett
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,11 @@
 // Arrays with all char sets
 var upperCaseOptions = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-// var upperCaseOptionsString = upperCaseOptions.toString();
 var lowerCaseOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-// var lowerCaseOptionsString = lowerCaseOptions.toString();
 var numberOptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; 
-// var numberOptionsString = numberOptions.toString();
 var specCharOptions = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '/', '}'];
-// var specCharOptionsString = specCharOptions.toString();
 
 // Variables for password options
 var selections = [];
-var selectionsString;
 var pwLength; 
 var upperCase; 
 var lowerCase; 
@@ -43,24 +38,18 @@ var generatePassword = function() {
   // add user choices to the selections array for use in generator
   if (upperCase) {
     selections = selections.concat(upperCaseOptions);
-    console.log(selections);
   }
   if (lowerCase) {
     selections = selections.concat(lowerCaseOptions);
-    console.log(selections);
   }
   if (number) {
     selections = selections.concat(numberOptions);
-    console.log(selections);
   }
   if (specChar) {
     selections = selections.concat(specCharOptions);
-    console.log(selections);
   }
-
-  // var selectionsString = selections.toString();
-  // console.log(selectionsString);
   
+  // Set Password outPut variable
   var passwordOutput = "";
 
   // Generate password
