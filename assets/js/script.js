1993b4716fee3dc84059b32de957a38115d9f20c
veritascoding <jarrettcoding@gmail.com>
2021-11-28 11:52:55 -0600

    move concat to generatePassword function and add no length conditional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,27 @@
-// need variable for inputs and characters
-
 // Arrays with all char sets
 var mainOptions = [
   ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
   ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'], 
   ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '/', '}']
-]
+];
 
 // Variables for password options
-var selections = []
+var selections = [];
 var pwLength; 
 var upperCase; 
 var lowerCase; 
 var number; 
 var specChar; 
 
-// add user choices to the selections array for use in generator
-if (upperCase) {selections.concat(mainOptions[0])}
-if (lowerCase) {selections.concat(mainOptions[1])}
-if (number) {selections.concat(mainOptions[2])}
-if (specChar) {selections.concat(mainOptions[3])}
 
 // Prompt user:
 var generatePassword = function() {
   pwLength = window.prompt("Please input a password length from 8 to 120.")
-  if (pwLength < 8) {
+  console.log(pwLength);
+  if (!pwLength) {
+    window.alert("You must enter a password length.")
+  } else if (pwLength < 8) {
     window.alert("You must select a minimum length of 8 characters.")
   } else if (pwLength > 120) {
     window.alert("You must select a length less than 120 characters.")
@@ -35,16 +31,37 @@ var generatePassword = function() {
     number = confirm("Would you liek numebrs?")
     specChar = confirm("Would you like special characters?")
   }
-}
 
-for (i = 0; i < pwLength; i++) {
+  // check if user selected at least one option
+  if (!upperCase && !lowerCase && !number && !specChar) {
+    window.alert("You must select at least one option!")
+  } 
+
+  // add user choices to the selections array for use in generator
+  if (upperCase) {
+    selections.concat(mainOptions[0]);
+    console.log(selections);
+  }
+  if (lowerCase) {
+    selections.concat(mainOptions[1]);
+    console.log(selections);
+  }
+  if (number) {
+    selections.concat(mainOptions[2]);
+    console.log(selections);
+  }
+  if (specChar) {
+    selections.concat(mainOptions[3]);
+    console.log(selections);
+  }
   
+  // Generate password
+  for (i = 0; i < pwLength; i++) {
+    return (selections[Math.floor(Math.random() * selections.length)]);
+  }
 }
 
 // Make a reset function
-
-// make a no values selected option
-
 
 
 
